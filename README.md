aabd57218d25f497f622929018e57973df789969
sahir <mohd.sahir@joget.com>
2026-01-06 15:12:21 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Overview
+
+This project provides an [AspectJ](https://www.eclipse.org/aspectj/) aspect that intercepts calls to/overrides `org.joget.plugin.enterprise.DateFormatter.format()` method to log more verbose info when exception is thrown.
+
+Additional info logged is:
+
+- App Id
+- App Version
+- DataList Id
+- row data
+
+Sample info logged in Monitor > System Logs:
+```
+DateFormatterAspect - appId=myApp, appVersion=1, dataList.id=list_of_records, {field1=test, id=2156c0cb-f23c-4591-828f-5c2bf24d62da, field3=2026/01/02, field2=test}
+```
+
+This info is useful to identify which database data is causing below exception pattern:
+```
+java.text.ParseException: Unparseable date: "2026/01/02"
+```
+
+## How to Use
+
+1. Build the jar file with `mvn clean install`.
+2. Copy the `target/date-formatter-aspect-9.0-SNAPSHOT.jar` file into `tomcat/webapps/jw/WEB-INF/lib`.
+3. Restart Joget/Tomcat
+
+## License
+
+This software is provided as-is without warranty. No liability is assumed for any damages resulting from the use of this code.
---
+++ src/main/java/org/joget/aspect/DateFormatterAspect.java
@@ -0,0 +1,88 @@
+package org.joget.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+import java.text.SimpleDateFormat;
+import java.time.chrono.ThaiBuddhistChronology;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.TemporalAccessor;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import org.joget.plugin.enterprise.DateFormatter;
+import org.joget.apps.datalist.model.DataList;
+import org.joget.apps.datalist.model.DataListColumn;
+import org.joget.apps.app.model.AppDefinition;
+import org.joget.apps.app.service.AppUtil;
+import org.joget.apps.form.lib.DatePicker;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.joget.commons.util.LogUtil;
+
+@Aspect
+public class DateFormatterAspect {
+
+    @Pointcut("execution(public String org.joget.plugin.enterprise.DateFormatter.format(..))")
+    public void format() {}
+
+    @Around("format()")
+    public Object aroundFormat(ProceedingJoinPoint pjp) throws Throwable {
+        AppDefinition appDef = AppUtil.getCurrentAppDefinition();
+        
+        // get params: [dataList, column, row, value]
+        Object[] args = pjp.getArgs();
+        DataList dataList = (DataList) args[0];
+        // DataListColumn column = (DataListColumn) args[1];
+        HashMap row = (HashMap) args[2];
+        String value = (String) args[3];
+
+        String result = "";
+
+        if (value != null && !value.isEmpty()) {
+            result = value;
+            try {
+                DateFormatter plugin = (DateFormatter) pjp.getTarget();
+                Locale userLocale = LocaleContextHolder.getLocale();
+                String dataFormatString = plugin.getPropertyString("dataFormat");
+
+                // getFormat() is not public
+                java.lang.reflect.Method getFormatMethod = DateFormatter.class.getDeclaredMethod("getFormat");
+                getFormatMethod.setAccessible(true);
+                String displayFormatString = (String) getFormatMethod.invoke(plugin);
+
+                if ("th".equals(userLocale.getLanguage()) && "TH".equals(userLocale.getCountry()) && !"true".equalsIgnoreCase(plugin.getPropertyString("dateStoreInUTC"))) {
+                    DateTimeFormatter inputFormatter = new DateTimeFormatterBuilder()
+                            .parseCaseInsensitive()
+                            .appendPattern(dataFormatString)
+                            .toFormatter(Locale.ENGLISH)
+                            .withChronology(ThaiBuddhistChronology.INSTANCE);
+                    TemporalAccessor parsed = inputFormatter.parse(result);
+                    DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern(displayFormatString, Locale.ENGLISH)
+                            .withChronology(ThaiBuddhistChronology.INSTANCE);
+                    result = outputFormatter.format(parsed);
+                } else {
+                    if ("true".equalsIgnoreCase(plugin.getPropertyString("dateStoreInUTC"))) {
+                        dataFormatString = DatePicker.UTC_DATEFORMAT;
+                    }
+                    SimpleDateFormat dataFormat = new SimpleDateFormat(dataFormatString, userLocale);
+                    SimpleDateFormat displayFormat = new SimpleDateFormat(displayFormatString, userLocale);
+                    if ("true".equalsIgnoreCase(plugin.getPropertyString("dateStoreInUTC"))) {
+                        dataFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+                        displayFormat.setTimeZone(LocaleContextHolder.getTimeZone());
+                    }
+                    Date date = dataFormat.parse(result);
+                    result = displayFormat.format(date);
+                }
+            } catch (Exception e) {
+                LogUtil.error("DateFormatterAspect", e, "appId=" + appDef.getAppId() + ", appVersion=" + appDef.getVersion() + ", dataList.id=" + dataList.getId() + ", " + row);
+            }
+        }
+        
+        return result;
+    }
+}
